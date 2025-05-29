24b34127fb6b61abc8439c7cf4629c98093ac1a6
MatiCarrera <mcarreradev12@gmail.com>
2025-05-29 16:38:02 -0300

    Implement get tag server action

--- app/(root)/tags/page.tsx
+++ app/(root)/tags/page.tsx
@@ -1,5 +1,16 @@
+import { getTags } from "@/lib/actions/tag.actions"
 
-const Tags = () => {
+const Tags = async () => {
+
+  const {success, data, error} = await getTags({
+    page: 1,
+    pageSize: 10,
+    query: "javascript ",
+  })
+
+  const { tags } = data || {};
+
+  console.log("Tags:", JSON.stringify(tags, null, 2));
   return (
     <div>Tags</div>
   )
