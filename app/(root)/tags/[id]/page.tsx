c5f9c745950d494b2e6958b41a6c7c3421cc1a3b
MatiCarrera <mcarreradev12@gmail.com>
2025-06-15 19:03:20 -0300

    filer tag id

--- app/(root)/tags/[id]/page.tsx
+++ app/(root)/tags/[id]/page.tsx
@@ -1,11 +1,12 @@
 import QuestionCard from '@/components/cards/QuestionCard';
 import DataRenderer from '@/components/DataRenderer';
+import CommonFilter from '@/components/filters/CommonFilter';
 import LocalSearch from '@/components/search/LocalSearch';
+import { AnswerFilters, TagFilters } from '@/constants/filters';
 import ROUTES from '@/constants/routes';
 import { EMPTY_QUESTION } from '@/constants/states';
 import { getTagQuestions } from '@/lib/actions/tag.action';
 
-import React from 'react'
 
 const Page = async ({params, searchParams}: RouteParams) => {
 
@@ -26,14 +27,18 @@ const Page = async ({params, searchParams}: RouteParams) => {
        <section className="flex flex-col-reverse sm:flex-row justify-between gap-4 sm:flex-grow sm:items-center">
         <h1 className="h1-bold text-dark100_light900">{tag?.name}</h1>
       </section>
-      <section className="mt-11">
+       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
         <LocalSearch
           route={ROUTES.TAG(id)}
           imgSrc="/icons/search.svg"
           placeholder="Encuentra preguntas"
           otherClasses="flex-1"
         />
-      </section>
+         <CommonFilter
+        filters={AnswerFilters}
+        otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
       <DataRenderer
         success={success}
         error={error}
