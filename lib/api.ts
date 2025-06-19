a5266cafa282698acd7f84af42b2893bf4c2f428
MatiCarrera <mcarreradev12@gmail.com>
2025-06-18 23:55:46 -0300

    readme

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,7 @@ import { IAccount } from "@/database/account.model";
 import ROUTES from "@/constants/routes";
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api" ;
 
 export const api = {
   auth: {
